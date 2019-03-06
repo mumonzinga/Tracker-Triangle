23f5701594dd988821fd2534f8f7f246c5507fea
Mumo Nzinga <mnzinga888@gmail.com>
2019-03-06 15:49:08 +0300

    correct mistakes

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,26 @@
-var trianglemeasure = function (side1, side2,side3) {
-  var side1= parseInt(document.getElementById('side1').value());
-  var side2= parseInt(document.getElementById('side2').value());
-  var side3 = parseInt(document.getElementById('side3').value());
-  var side = {  side1;
-                side2;
-                side3;
-  };
-
-
-  if((side1+side2)<=side3)  ||  ((side2+side3)<=side1)  ||  ((side1+side3)<=side2) {
-    return "not-triangle";
-  } else if (side1 === side2 && side2 === side3){
-    return "equilateral";
-  } else if (side1 === side2 || side2 === side3 || side3 === side1) {
-    return "isosceles";
-  } else if(side1 != side2 && side2 != side3 && side1 != side3) {
-    return "scalene";
-  }
-});
-
-
-
-$(document).ready(function()  {
-  $("form#trianglemeasure").submit(function(event)  {
-
-
-   side1=parseInt($("input#side1").value());
-   side2=parseInt($("input#side2").value());
-   side3=parseInt($("input#side3").value());
-   var result = trianglemeasure(side1, side2, side3);
-  if(result === "not-triangle") {
-   $("#not-triangle").show();
- } else if (result === equilateral) {
-    $("#equilateral").show();
-  } else if (result === "isosceles") {
-    $("#isosceles").show();
-  } else if (result === "scalene") {
-    $("#scalene").show();
-  }
-  $("#result").append();
-  event.preventDefault();
-
-  });
-});
+function trianglemeasure() {
+var inp1=document.getElementById("side1").value;
+var inp2=document.getElementById("side2").value;
+var inp3=document.getElementById("side3").value;
+var sideA=parseInt(inp1);
+var sideB=parseInt(inp2);
+var sideC=parseInt(inp3);
+if (sideA === sideB && sideB === sideC && sideC === sideA){
+  alert("EQUILATERAL TRAIANGLE");
+}
+else if (sideA === sideB && sideA !== sideC) {
+  alert("ISOSCELES TRIANGLE");
+}else if (sideA === sideC && sideA !== sideB) {
+  alert("ISOSCELES TRAIANGLE");
+}else if (sideB === sideC && sideB !== sideA) {
+  alert("ISOSCELES  TRAIANGLE");
+}else if (sideA !== sideB && sideB !== sideC && sideA !== sideC && sideA + sideB > sideC && sideB + sideC > sideA){
+  alert("SCALENE TRIANGLE");
+}else if (sideA + sideB <= sideC) {
+  alert ("NOT A TRIANGLE");
+}else if (sideA + sideC <=sideB) {
+  alert ("NOT A TRIANGLE");
+}else if (sideB + sideC <=sideA)  {
+  alert ("NOT A TRIANGLE");
+}
+}
