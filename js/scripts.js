29e219ec5bf51f14389594b5cdf5b725c15a39e5
Mumo Nzinga <mnzinga888@gmail.com>
2019-03-06 07:03:27 +0300

    add final changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,45 @@
-var side1;
-var side2;
-var side3;
+var trianglemeasure = function (side1, side2,side3) {
+  var side1= parseInt(document.getElementById('side1').value());
+  var side2= parseInt(document.getElementById('side2').value());
+  var side3 = parseInt(document.getElementById('side3').value());
+  var side = {  side1;
+                side2;
+                side3;
+  };
 
-$(document).ready(function() {
+
+  if((side1+side2)<=side3)  ||  ((side2+side3)<=side1)  ||  ((side1+side3)<=side2) {
+    return "not-triangle";
+  } else if (side1 === side2 && side2 === side3){
+    return "equilateral";
+  } else if (side1 === side2 || side2 === side3 || side3 === side1) {
+    return "isosceles";
+  } else if(side1 != side2 && side2 != side3 && side1 != side3) {
+    return "scalene";
+  }
+});
+
+
+
+$(document).ready(function()  {
   $("form#trianglemeasure").submit(function(event)  {
+
+
    side1=parseInt($("input#side1").value());
    side2=parseInt($("input#side2").value());
    side3=parseInt($("input#side3").value());
-
-  if((side1+side2)<=side3)  ||  ((side2+side3)<=side1)  ||  ((side1+side3)<=side2) {
-    $('.answer').hide();
-    $('#not-triangle').show();
-  } else if (side1 === side2 && side2 === side3){
-    $('answer').hide();
-    $('#equilateral').show();
-  } else if (side1 === side2 || side2 === side3 || side3 === side1) {
-    $('.answer').hide();
-    $('#isosceles').show();
-  } else if(side1 != side2 && side2 != side3 && side1 != side3) {
-    $('.answer').hide();
-    $('#scalene').show();
+   var result = trianglemeasure(side1, side2, side3);
+  if(result === "not-triangle") {
+   $("#not-triangle").show();
+ } else if (result === equilateral) {
+    $("#equilateral").show();
+  } else if (result === "isosceles") {
+    $("#isosceles").show();
+  } else if (result === "scalene") {
+    $("#scalene").show();
   }
-  event.preventDefault ();;
+  $("#result").append();
+  event.preventDefault();
+
   });
 });
